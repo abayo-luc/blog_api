e612ec1fb9ebacf48915cc25b0da49a728207dfa
Luc Abayo <luc.bayo@gmail.com>
2020-04-12 18:45:07 +0200

    :boom: filter by year (#10)

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -3,7 +3,9 @@ import MainController from './main';
 class CategoryController {
 	static async index(req, res) {
 		try {
-			const { count, rows } = await db.Category.findAndCountAll();
+			const { count, rows } = await db.Category.findAndCountAll({
+				order: [['updatedAt', 'DESC']],
+			});
 			return res.status(200).json({ count, categories: rows });
 		} catch (error) {
 			return MainController.handleError(res, error);
