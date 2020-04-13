7cb30ccb07359e5da0ed7b581aed0647bb5018d0
Luc Abayo <luc.bayo@gmail.com>
2020-04-13 14:03:09 +0200

    :boom: get user ip info (#12)

--- src/routes/user.routers.js
+++ src/routes/user.routers.js
@@ -3,4 +3,5 @@ import UserController from '../controllers/UserController';
 const userRouters = Router();
 userRouters.get('/users', UserController.index);
 userRouters.get('/users/:id', UserController.find);
+userRouters.get('/user_ip_info', UserController.userInfo);
 export default userRouters;
