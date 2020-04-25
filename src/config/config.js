79e06df4b87966b04c566006e7b235f8416f46b8
Luc Abayo <luc.bayo@gmail.com>
2020-04-25 19:05:27 +0200

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -19,11 +19,6 @@ module.exports = {
 		logging: false,
 	},
 	production: {
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
-		host: process.env.HOST,
-		dialect: 'postgres',
-		logging: false,
-	},
+		use_env_variable: 'DATABASE_URL',
+	}
 };
