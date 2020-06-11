10dedf252bf74a50dea1d311e65a68f474c4ccb7
Luc <luc.bayo@gmail.com>
2020-06-12 01:23:49 +0200

    fix data counter

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -1,16 +1,16 @@
-import db from '../models';
-import MainController from './main';
+import db from "../models";
+import MainController from "./main";
 class CategoryController {
-	static async index(req, res) {
-		try {
-			const { count, rows } = await db.Category.findAndCountAll({
-				order: [['updatedAt', 'DESC']],
-			});
-			return res.status(200).json({ count, categories: rows });
-		} catch (error) {
-			return MainController.handleError(res, error);
-		}
-	}
+  static async index(req, res) {
+    try {
+      const { count, rows } = await db.Category.findAndCountAll({
+        order: [["updatedAt", "DESC"]],
+      });
+      return res.status(200).json({ count, categories: rows });
+    } catch (error) {
+      return MainController.handleError(res, error);
+    }
+  }
 }
 
 export default CategoryController;
--- src/utils/queryHelper.js
+++ src/utils/queryHelper.js
@@ -1,22 +1,22 @@
-import { Op } from 'sequelize';
+import { Op } from "sequelize";
 
-export const dataSort = (order) => [['updatedAt', 'ASC'], ...order];
+export const dataSort = (order) => [["updatedAt", "ASC"], ...order];
 export const textSearch = (text, fields) => {
-	if (text) {
-		return {
-			[Op.or]: fields.map((item) => ({
-				[item]: {
-					[Op.iLike]: `%${text}%`,
-				},
-			})),
-		};
-	}
-	return {};
+  if (text) {
+    return {
+      [Op.or]: fields.map((item) => ({
+        [item]: {
+          [Op.iLike]: `%${text}%`,
+        },
+      })),
+    };
+  }
+  return {};
 };
 export const paginate = ({ page = 1, limit = 50 }) => {
-	const offset = (Number(page) - 1) * limit;
-	return {
-		offset,
-		limit,
-	};
+  const offset = (Number(page) - 1) * limit;
+  return {
+    offset,
+    limit,
+  };
 };
